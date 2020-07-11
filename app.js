2c57d91573cff36720a8f4ab2e5721280f40a111
captainkuk <naicaptiankuk@gmail.com>
2020-07-11 20:35:22 +0700

    add socket-io

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
     res.sendfile('index.html');
+});
+
+
+io.on('connection',function(socket){
+    
+    //Eventer user connect
+    console.log('Connected');
+    socket.on('disconnect',function(){
+        console.log('Disconnected')
+    });
+
 });
 
 http.listen(3080,function(){
