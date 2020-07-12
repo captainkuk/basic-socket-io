01a5d33cf0afe644a083716f0fe8fa5040aee19e
captainkuk <naicaptiankuk@gmail.com>
2020-07-12 10:51:55 +0700

    complete project

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ app.get('/',function(req,res){
 
 users = [];
 io.on('connection',function(socket){
+    
     console.log('Connected');
+    
     socket.on('setUsername',function(data){
+
+        console.log(data);
+        //ค้นหา user ใน array users ถ้าเจอจะ return เป็นค่า index
         if(users.indexOf(data)>-1){
-            users.push(data);
-            socket.emit(userSet,{username:data});
+            socket.emit('userExists',data+' มีผู้ใช้ชื่อนี้แล้ว กรุณาใช้ชื่ออื่น');
         }else{
-            socket.eimit('userExists',data+' มีผู้ใช้ชื่อนี้แล้ว กรุณาใช้ชื่ออื่น');
+            users.push(data);
+            socket.emit('userSet',{username:data});
         }
+    });
+    
+    socket.on('msg',function(data){
+        io.sockets.emit('newmsg',data);
     });
 });
 
